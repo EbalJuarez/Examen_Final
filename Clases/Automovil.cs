d202b3439fbdf49503190eed38a3c56133d79be0
Ebal Juárez <ebaljuarez_7@umes.edu.gt>
2025-06-09 08:56:33 -0600

    Se creo el formulario

--- Clases/Automovil.cs
+++ Clases/Automovil.cs
@@ -1,8 +1,12 @@
-﻿namespace Examen_Final.Clases
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Examen_Final.Clases
 {
     public class Automovil : Vehiculo
     {
+        [Required]
         public int numero_puertas {  get; set; }
+        [Required]
         public string? tipo_combustible { get; set; }
     }
 }
--- Clases/Vehiculo.cs
+++ Clases/Vehiculo.cs
@@ -1,10 +1,15 @@
-﻿namespace Examen_Final.Clases
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Examen_Final.Clases
 {
     public class Vehiculo
     {
+        [Required]
         public string? numero_chasis { get; set; }
+        [Required]
         public string? Marca { get; set; }
-        public DateOnly? date_only { get; set; }
+        [Required]
+        public string? año_fabricacion { get; set; }
 
     }
 }
